c76b195b498b72dc19990435a7dd7968a95e4b53
Jadav Akshara <jadavakshara29@gmail.com>
2026-08-23 15:58:51 +0530

    Create Basic_arithmetic.py

---
+++ 1-Basics/Basic_arithmetic.py
@@ -0,0 +1,10 @@
+a = int(input("Enter a number:"))
+b = int(input("Enter b number:"))
+sum = a + b
+diff = a - b
+product = a * b
+division = a / b
+print(sum)
+print(diff)
+print(product)
+print(division)
