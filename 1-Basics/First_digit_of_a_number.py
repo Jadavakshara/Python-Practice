922375ab35d11503735ab5ec76fb6390a70c2ba9
Jadav Akshara <jadavakshara29@gmail.com>
2026-08-25 12:44:43 +0530

    Create First_digit_of_a_number.py

---
+++ 1-Basics/First_digit_of_a_number.py
@@ -0,0 +1,15 @@
+# get first digit of a number
+num = int(input())
+print(num // 100)
+
+# get last 2 digits
+num = int(input())
+print(num % 100)
+
+# Remove last digit
+num = int(input())
+print(num // 10)
+
+# Remove last 2 digit
+num = int(input())
+print(num // 100)
