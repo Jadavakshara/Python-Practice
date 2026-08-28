1d1fc4b5a0f790d761b2ea6ed8aff5084f473ed2
Jadav Akshara <jadavakshara29@gmail.com>
2026-08-28 23:28:56 +0530

    Create input_&_output_problems.py

---
+++ 1-Basics/input_&_output_problems.py
@@ -0,0 +1,11 @@
+# Take two number and print their sum
+a, b =map(int, input().split())
+print(a + b)
+
+# Take multiple integers and print their sum
+arr = list(map(int, input().split()))
+print(sum(arr))
+
+# Take three number and print their average
+a, b, c = map(int, input().split())
+print((a+b+c) / 3)
