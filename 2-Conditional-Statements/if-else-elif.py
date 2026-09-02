4880d69aa7c3a92bf35651626076e57332aa2024
Jadav Akshara <jadavakshara29@gmail.com>
2026-09-02 16:00:40 +0530

    Create if-else-elif.py

---
+++ 2-Conditional-Statements/if-else-elif.py
@@ -0,0 +1,10 @@
+Biryani = input("Famous Hyderabadi biryani")
+
+if Biryani == "Paradise":
+    print("Spicy & tasty")
+elif Biryani == "Shahdab":
+    print("double masala")
+elif Biryani == "road side":
+    print("budget friendly")
+else:
+    print("PG Biryani")
